587531917cb36f8af3177184c0b3a30249f41b64
Karishtan <52981138+Karishtan@users.noreply.github.com>
2021-06-28 13:37:56 -0700

    All Routes

--- routes/companies.js
+++ routes/companies.js
@@ -11,16 +11,40 @@ router.get('/', async (req, res) => {
     return res.json(results.rows)
 })
 
-// Code Search Route don't use to prevent (SQL Injection attack)
+// ****Code Search Route don't use to prevent (SQL Injection attack)****
+// router.get('/search', async (req, res, next) => {
+//     try {
+//         const { code } = req.query;
+//         const results = await db.query(`SELECT * FROM companies WHERE code='${code}'`)
+//         return res.json(results.rows)
+//     } catch (e) {
+//         return next(e)
+//     }
+// })
+
+// Code Search(USE THIS)
 router.get('/search', async (req, res, next) => {
     try {
         const { code } = req.query;
-        const results = await db.query(`SELECT * FROM companies WHERE code='${code}'`)
+        const results = await db.query(`SELECT * FROM companies WHERE code=$1`, [code])
         return res.json(results.rows)
     } catch (e) {
         return next(e)
     }
 })
+
+
+
+// Add a company 
+router.post('/', async (req, res, next) => {
+    try {
+        const { name, code } = req.body;
+        const results = await db.query('INSERT INTO users (name, code) VALUES()')
+    } catch (e) {
+
+    }
+})
+
 
 
 
